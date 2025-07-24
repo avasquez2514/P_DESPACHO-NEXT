a3be8fa632fc8ac18b29c4755611897f16eac59b
Anderson <anderzon.0077@gmail.com>
2025-07-23 20:14:27 -0500

    Fix: forzar IPv4 y sslmode en conexión a Supabase1

--- Backend/db.js
+++ Backend/db.js
@@ -1,12 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL + "?sslmode=require",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
-  // ⚠️ Agrega explícitamente el host IPv4 para evitar IPv6
-  host: "db.uwkqbjvuuiudbinvwvnf.supabase.co", // este es tu host de Supabase
 });
 
 module.exports = pool;
