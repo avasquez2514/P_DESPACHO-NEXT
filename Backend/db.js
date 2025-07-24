fe36a8346f3d6a6e162656d7185ebe7d4f3d1a49
Anderson <anderzon.0077@gmail.com>
2025-07-23 19:22:06 -0500

    Conectar backend a Supabase con DATABASE_URL

--- Backend/db.js
+++ Backend/db.js
@@ -1,10 +1,9 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
-// Si estás usando Render, usa directamente DATABASE_URL
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // Necesario para conexiones externas con Supabase
+    rejectUnauthorized: false, // Necesario para Supabase
   },
 });
 
